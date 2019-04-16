3aeb330b9ad6fdcb3e2c2e85d43df9d8735b1e91
Junaid <jsolkar@consenteye.com>
2019-04-16 23:30:07 +0100

    added form and weather component

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
+
+const API_KEY = "889a5ef41ebfe5408f85f8b1883567dd";
 class App extends React.Component{
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const api_call = await fetch(`http://openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`, {mode: "no-cors"})
+    
+    const data = await api_call.json();
+
+    console.log(data)
+  }
   render(){
     return (
       <div>
         <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
