334f450aa43bc0d2e65c7023e6ecb9155e232805
Junaid <jsolkar@consenteye.com>
2019-04-18 23:35:08 +0100

    added styling to app with bootstrap and css

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -3,8 +3,8 @@ import React from "react";
 const Titles = () =>(
 
       <div>
-          <h1>Weather Finder</h1>
-          <p>Find out about the lates weather information and more ...</p>
+          <h1 className="title-container__title">Weather Finder</h1>
+          <p className="title-container__subtitle">Find out about the lates weather information and more ...</p>
       </div>
 
 );
