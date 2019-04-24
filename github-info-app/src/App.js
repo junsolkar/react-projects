4892a4c00ec9583d31edf30f9efe36f113c3e64e
Junaid <jsolkar@consenteye.com>
2019-04-24 23:43:28 +0100

    created UserForm component

--- github-info-app/src/App.js
+++ github-info-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import UserForm from './components/UserForm'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Github Info App</h1>
       </header>
+      <UserForm />
     </div>
   );
 }
