7c7c3c441e948eaea23bf37fa78f221912c1fce1
Junaid <jsolkar@consenteye.com>
2019-04-25 23:37:37 +0100

    axios call to display github info

--- github-info-app/src/App.js
+++ github-info-app/src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
 
 import UserForm from './components/UserForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Github Info App</h1>
-      </header>
-      <UserForm />
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    repos: null
+  }
+
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    if (user) {
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        const repos = res.data.public_repos;
+        this.setState({repos})
+      })
+    } else return
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Github Info App</h1>
+        </header>
+        <UserForm getUser={this.getUser} />
+        {this.state.repos ? <p>number of repos is : {this.state.repos}</p> : <p>please enter a username</p>}
+      </div>
+    );
+  }
+
 }
 
 export default App;
