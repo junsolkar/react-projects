b6840823002765f3a76fa7a587880ef7cb8c1987
Junaid <jsolkar@consenteye.com>
2019-04-30 22:37:22 +0100

    capturing item from input

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import uuid from 'uuid'
 
-function App() {
-  return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-10 mx-auto col-md-8 mt-4'>
-        <h3 className='text-capitalize text-center'>Todo Input</h3>
-          <TodoInput />
-          <TodoList />
+class App extends Component {
+  state = {
+    items:[],
+    id:uuid(),
+    item:'',
+    editItem:false
+  };
+
+  handleChange = e =>{
+    this.setState({
+      item:e.target.value
+    })
+  };
+  handleSubmit = e =>{
+    e.preventDefault()
+
+    const newItem = {
+      id:this.state.id,
+      item:this.state.item
+    }
+    console.log("newItem", newItem)
+    const updateItems = [...this.state.items, newItem]
+
+    this.setState({
+      items:updateItems,
+      item:'',
+      id:uuid(),
+      editItem: false
+    })
+  };
+
+
+  render(){
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-8 mt-4'>
+          <h3 className='text-capitalize text-center'>Todo Input</h3>
+            <TodoInput 
+            item={this.state.item} 
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}/>
+            <TodoList />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
--- todo-list/src/components/TodoInput.js
+++ todo-list/src/components/TodoInput.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
   render() {
+    const {item, handleChange, handleSubmit} = this.props
     return (
       <div className="card card-body my-3">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
-          <div classname="input-group-prepend">
+          <div className="input-group-prepend">
           <div className="input-group-text bg-primary text-white">
-          <span className="add-on"><i className="fas fa-book"></i></span>
+          <i className="fas fa-book"></i>
           </div>
           </div>
-          <input type="text" className="form-control text-capitalize" placeholder="add a todo list item"/>
+          <input 
+          type="text" 
+          className="form-control text-capitalize" 
+          placeholder="add a todo list item" 
+          value={item} 
+          onChange={handleChange}/>
           </div>
           <button type="submit" className="btn btn-block btn-primary mt-3">add item</button>
         </form>
