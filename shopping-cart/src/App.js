8b8d5bc809b60633b6e4b69f1ddbe13c990f1f8f
Junaid <jsolkar@consenteye.com>
2019-05-01 22:41:50 +0100

    subtotal component

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+
+import SubTotal from './components/Subtotal/Subtotal'
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 200
+    }
+  }
+ 
   render(){
     return (
-      <div>
+      <div className="container">
+        <Container className="purchase-card">
+          <SubTotal price={this.state.total.toFixed(2)} />
+        </Container>
       </div>
     );
   }
