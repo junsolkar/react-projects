d0db92b9d4159e7430219d36a8d360c221c2431e
Junaid <jsolkar@consenteye.com>
2019-05-01 22:07:29 +0100

    added edit and delete functionality

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -42,6 +42,24 @@ class App extends Component {
     })
   }
 
+  handleDelete = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id)
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+  handleEdit = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id)
+    const selectedItems = this.state.items.find(item => item.id === id)
+    this.setState({
+      items: filteredItems,
+      item: selectedItems.title,
+      editItem: true,
+      id:id
+    })
+  }
+
   render(){
     return (
       <div className='container'>
@@ -49,10 +67,16 @@ class App extends Component {
           <div className='col-10 mx-auto col-md-8 mt-4'>
           <h3 className='text-capitalize text-center'>Todo Input</h3>
             <TodoInput 
-            item={this.state.item} 
+            item={this.state.item}
+            editItem={this.state.editItem}
             handleChange={this.handleChange}
             handleSubmit={this.handleSubmit}/>
-            <TodoList items={this.state.items} clearList={this.clearList}/>
+            <TodoList 
+            items={this.state.items} 
+            clearList={this.clearList} 
+            handleDelete={this.handleDelete}
+            handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react'
 import TodoItem from './TodoItem'
 export default class TodoList extends Component {
   render() {
-    const {items, clearList} = this.props
+    const {items, clearList, handleDelete, handleEdit} = this.props
     return (
       <ul className="list-group my-5">
         <h3 className="text-capitalize text-center">to do list</h3>
         {
           items.map(item => {
             return(
-              <TodoItem key={item.id} title={item.title}/>
+              <TodoItem 
+              key={item.id} 
+              title={item.title} 
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+              />
             )
           })
         }
