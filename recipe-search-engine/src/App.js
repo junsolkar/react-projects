02eb87f84994536abe3e00064161f8d551651fdf
Junaid <jsolkar@consenteye.com>
2019-05-12 20:43:56 +0100

    getRecipes method

--- recipe-search-engine/src/App.js
+++ recipe-search-engine/src/App.js
@@ -10,7 +10,25 @@ class App extends Component {
     recipes:[],
     url: "https://www.food2fork.com/api/search?key=5a84e3f5e7686b5a63954ee059213ae8"
   }
+  async getRecipes(){
+      try {
+        const data = await fetch(this.state.url)
+        const jsonData =  await data.json()
+    
+        this.setState({
+          recipes: jsonData.recipes
+        });
+      }catch (error){
+        console.log(error)
+      }
+  }
+
+  componentDidMount(){
+    this.getRecipes()
+  }
+
   render(){
+    console.log(this.state.recipes)
     return (
       <React.Fragment>
         <RecipeList />
