2494d8e82589953d6a0c8edd7efda1a36fb6bfb6
Junaid <jsolkar@consenteye.com>
2019-05-12 21:49:29 +0100

    recipe list component

--- recipe-search-engine/src/App.js
+++ recipe-search-engine/src/App.js
@@ -7,31 +7,31 @@ import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
   state = {
-    recipes:[],
+    recipes: recipes,
     url: "https://www.food2fork.com/api/search?key=5a84e3f5e7686b5a63954ee059213ae8"
   }
-  async getRecipes(){
-      try {
-        const data = await fetch(this.state.url)
-        const jsonData =  await data.json()
+  // async getRecipes(){
+  //     try {
+  //       const data = await fetch(this.state.url)
+  //       const jsonData =  await data.json()
     
-        this.setState({
-          recipes: jsonData.recipes
-        });
-      }catch (error){
-        console.log(error)
-      }
-  }
+  //       this.setState({
+  //         recipes: jsonData.recipes
+  //       });
+  //     }catch (error){
+  //       console.log(error)
+  //     }
+  // }
 
-  componentDidMount(){
-    this.getRecipes()
-  }
+  // componentDidMount(){
+  //   this.getRecipes()
+  // }
 
   render(){
     console.log(this.state.recipes)
     return (
       <React.Fragment>
-        <RecipeList />
+        <RecipeList recipes={this.state.recipes}/>
         <RecipeDetails />
       </React.Fragment>
     );
