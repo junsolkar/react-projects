d6914dd2d7bd25dd0d091e67361ed2eaeba4e144
Junaid <jsolkar@consenteye.com>
2019-05-16 20:47:23 +0100

    recipe search

--- recipe-search-engine/src/components/RecipeSearch.js
+++ recipe-search-engine/src/components/RecipeSearch.js
@@ -4,9 +4,29 @@ export default class RecipeSearch extends Component {
   render() {
     return (
       <React.Fragment>
-      <h1>
-        From Recipe Search
-      </h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-5 mt-5 text-center">
+              <h1 className="text-slanted text-capitalize">
+              search for recipe list with{" "}
+                <strong className="text-danger">Food2Ffor</strong>
+              </h1>
+              <form className="mt-4">
+                <label htmlFor="search" className="text-capitalize">
+                type recipes seperated by comma
+                </label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    name="search"
+                    placeholder="beef,onions,tomatos,chicken,noodles"
+                    className="form-control"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     )
   }
